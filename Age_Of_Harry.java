f8415c4f1bfa99664ae84c1edb5a45a7eff5e360
M.S.Adhitya <adhitya.senthil22@gmail.com>
2026-03-11 10:50:46 +0530

    Level-1 q1 ans

--- Age_Of_Harry.java
+++ Age_Of_Harry.java
@@ -2,7 +2,7 @@ public class Age_Of_Harry {
     public static void main(String[] args){
         int birthYear = 2000;
         int currentYear = 2024;
-        string name = "Harry";
+        String name = "Harry";
         int age = currentYear - birthYear;
         System.out.println(name + " is " + age + " years old.");
     }}
