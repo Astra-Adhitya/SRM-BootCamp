6e1811709aff111fb2256a13796d7d84a9576f07
M.S.Adhitya <adhitya.senthil22@gmail.com>
2026-03-11 10:47:00 +0530

    New Branch Week1&2/Level-1 and q1 ans

---
+++ Age_Of_Harry.java
@@ -0,0 +1,8 @@
+public class Age_Of_Harry {
+    public static void main(String[] args){
+        int birthYear = 2000;
+        int currentYear = 2024;
+        string name = "Harry";
+        int age = currentYear - birthYear;
+        System.out.println(name + " is " + age + " years old.");
+    }}
